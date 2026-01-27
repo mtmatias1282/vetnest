0f35e32c179f9c1c55d4e910ef0f7ec683230dd8
Matías Mejía Terán <matias.mejia@epn.edu.ec>
2026-01-27 13:34:54 -0500

    feat: enhance CreateProductDto with validation and add ProductService dependencies

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,26 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Category } from 'src/categories/entities/category.entity';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'El nombre del producto es obligatorio' })
+  @IsString({ message: 'El nombre del producto no es válido' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El precio del producto es obligatorio' })
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'El precio del producto no es un número válido' },
+  )
+  price: number;
+
+  @IsNotEmpty({ message: 'El inventario del producto no puede ir vacio' })
+  @IsNumber(
+    { maxDecimalPlaces: 0 },
+    { message: 'El inventario del producto no es un número válido' },
+  )
+  inventory: number;
+
+  @IsNotEmpty({ message: 'La categoría del producto es obligatoria' })
+  @IsInt({ message: 'La categoría del producto no es válida' })
+  categoryId: number;
+}
